ecc8b05e8031a372c88cfd5f8e5220d14a882bb6
koladeKA <kolakinolakolade@gmail.com>
2020-02-28 16:18:07 +0100

    changes in about page

---
+++ about.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap css  -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <!-- Slick css -->
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/slick-theme.css">
+    <!-- main css file  -->
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <title> Aiiburtel About-page</title>
+</head>
+<body>
+  <div id="top"></div>
+  <?php include "include/header.html"?>
+  <section class="about-hero">
+    <div class="container">
+      <div class="row m-0">
+        <div class="col-md-6"></div>
+        <div class="col-md-6">
+          <img src="assets/hero-img/hero-img.png" class="img-fluid" width=""  alt="">
+        </div>
+      </div>
+    </div>
+  </section>
+
+    <!-- jquery .js -->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/slick.min.js"></script>
+    <!-- bootstrap .js -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>   
+</body>
+</html>
