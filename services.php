a2581765611efb60cf2b43bd6e1dc415d6a5d6bc
shammah nei <shammahk1@gmail.com>
2020-03-02 14:20:01 +0100

    started services page

---
+++ services.php
@@ -0,0 +1,429 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap css  -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <!-- Slick css -->
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/slick-theme.css">
+    <script src="https://code.iconify.design/1/1.0.4/iconify.min.js"></script>
+    <!-- main css file  -->
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    <link rel="shortcut icon" href="assets/aii logo/aii logo.png" type="image/x-icon">
+    <title> Aiiburtel - Services</title>
+</head>
+
+<body>
+  <div id="top"></div>
+  <?php include "include/header.html"?>
+  
+  <section class="container-fluid bg-black py-5 mt-90">
+    <div class="d-flex justify-content-center align-items-center py-5">
+      <h1 class="f-gt c-white my-5">
+        Our Services
+      </h1>
+    </div>
+  </section>
+
+  <!-- Services -->
+
+  <section class="py-5">
+      <div class="row m-0">
+        <div class="mx-auto">
+          <div class="heading d-flex align-items-center px-2 mx-auto mb-5">
+              <div class="line"></div>
+              <h3 >What We Do</h3>
+          </div>
+          <div class="container">
+            <div class="row mx-0 justify-content-center">
+              <div class="col-md-6 col-lg-4 mb-4">
+                <div class=" service-card bg-white pt-5 pb-5 px-4">
+                  <img src="./assets/icons/automation.svg" alt="System Automation" class="img-fluid icons mb-4 mt-3">
+                  <h5 class="title mb-3">
+                    System Automation
+                  </h5>
+                  <p class="mb-5">
+                    Bespoke Websites and Web Apps that are tailored to your individual needs. We listen to you and apply experience and expertise to creating a masterpiece you will be proud to call your own.
+                  </p>
+                    <a href="" class="cta-btn ">
+                      Bring Idea To Life
+                    </a>
+                </div>
+  
+              </div>
+              
+              <div class="col-md-6 col-lg-4 mb-4">
+                <div class=" service-card bg-white pt-5 pb-5 px-4">
+                  <img src="./assets/icons/machine-learning.svg" alt="Machine Learning" class="img-fluid icons mb-4 mt-3">
+                  <h5 class="title mb-3">
+                    Machine Learning
+                  </h5>
+                  <p class="mb-5">
+                    Bespoke Websites and Web Apps that are tailored to your individual needs. We listen to you and apply experience and expertise to creating a masterpiece you will be proud to call your own.
+                  </p>
+                    <a href="" class="cta-btn ">
+                      Bring Idea To Life
+                    </a>
+                </div>
+  
+              </div>
+              
+              <div class="col-md-6 col-lg-4 mb-4">
+                <div class=" service-card bg-white pt-5 pb-5 px-4">
+                  <img src="./assets/icons/artificial-intelligence.svg" alt="Artificial Intelligence" class="img-fluid icons mb-4 mt-3">
+                  <h5 class="title mb-3">
+                    Artificial Intelligence
+                  </h5>
+                  <p class="mb-5">
+                    Bespoke Websites and Web Apps that are tailored to your individual needs. We listen to you and apply experience and expertise to creating a masterpiece you will be proud to call your own.
+                  </p>
+                    <a href="" class="cta-btn">
+                      Bring Idea To Life
+                    </a>
+                </div>
+  
+              </div>
+              
+              <div class="col-md-6 col-lg-4 mb-4">
+                <div class=" service-card bg-white pt-5 pb-5 px-4">
+                  <img src="./assets/icons/automation.svg" alt="System Automation" class="img-fluid icons mb-4 mt-3">
+                  <h5 class="title mb-3">
+                    System Automation
+                  </h5>
+                  <p class="mb-5">
+                    Bespoke Websites and Web Apps that are tailored to your individual needs. We listen to you and apply experience and expertise to creating a masterpiece you will be proud to call your own.
+                  </p>
+                    <a href="" class="cta-btn ">
+                      Bring Idea To Life
+                    </a>
+                </div>
+  
+              </div>
+              
+              <div class="col-md-6 col-lg-4 mb-4">
+                <div class=" service-card bg-white pt-5 pb-5 px-4">
+                  <img src="./assets/icons/machine-learning.svg" alt="Machine Learning" class="img-fluid icons mb-4 mt-3">
+                  <h5 class="title mb-3">
+                    Machine Learning
+                  </h5>
+                  <p class="mb-5">
+                    Bespoke Websites and Web Apps that are tailored to your individual needs. We listen to you and apply experience and expertise to creating a masterpiece you will be proud to call your own.
+                  </p>
+                    <a href="" class="cta-btn ">
+                      Bring Idea To Life
+                    </a>
+                </div>
+  
+              </div>
+              
+              <div class="col-md-6 col-lg-4 mb-4">
+                <div class=" service-card bg-white pt-5 pb-5 px-4">
+                  <img src="./assets/icons/artificial-intelligence.svg" alt="Artificial Intelligence" class="img-fluid icons mb-4 mt-3">
+                  <h5 class="title mb-3">
+                    Artificial Intelligence
+                  </h5>
+                  <p class="mb-5">
+                    Bespoke Websites and Web Apps that are tailored to your individual needs. We listen to you and apply experience and expertise to creating a masterpiece you will be proud to call your own.
+                  </p>
+                    <a href="" class="cta-btn">
+                      Bring Idea To Life
+                    </a>
+                </div>
+  
+              </div>
+              
+            </div>
+          </div>
+        </div>
+          
+      </div>
+  </section>
+
+  <!-- Products -->
+
+  <section class=" py-5">
+      <div class="row m-0  bg-light-half ">
+        <div class="heading d-flex align-items-center mb-5 ml-auto">
+            <div class="line"></div>
+            <h3 >Some Of Our Products</h3>
+        </div>
+        <div class="container-fluid row mx-0 justify-content-center">
+          <div class="col-md-6 col-lg-4 mb-4">
+            <div class=" service-card product bg-white d-flex flex-column justify-content-end pt-5 pb-5 px-4">
+             
+              <h5 class="title  mb-3">
+                System Automation
+              </h5>
+              <p class="mb-5">
+                Bespoke Websites and Web Apps that are tailored to your individual needs. We listen to you and apply experience and expertise to creating a masterpiece you will be proud to call your own.
+              </p>
+              <div>
+                <a href="" class="cta-btn ">
+                  Bring Idea To Life
+                </a>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-4">
+            <div class=" service-card product bg-white   pt-5 pb-5 px-4">
+              
+              <h5 class="title mb-3">
+                System Automation
+              </h5>
+              <p class="mb-5">
+                Bespoke Websites and Web Apps that are tailored to your individual needs. We listen to you and apply experience and expertise to creating a masterpiece you will be proud to call your own.
+              </p>
+              <div>
+                <a href="" class="cta-btn ">
+                  Bring Idea To Life
+                </a>
+              </div>
+            </div>
+
+          </div>
+          
+          
+          
+          <div class="col-sm-0 col-md-0 col-lg-4 mb-4">
+            
+          </div>
+          
+          <div class="col-md-6 col-lg-4 mb-4">
+            <div class=" service-card product bg-white d-flex flex-column justify-content-end pt-5 pb-5 px-4">
+              
+              <h5 class="title mb-3">
+                System Automation
+              </h5>
+              <p class="mb-5">
+                Bespoke Websites and Web Apps that are tailored to your individual needs. We listen to you and apply experience and expertise to creating a masterpiece you will be proud to call your own.
+              </p>
+              <div>
+                <a href="" class="cta-btn ">
+                  Bring Idea To Life
+                </a>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-4">
+            <div class=" service-card product bg-white pt-5 pb-5 px-4">
+              
+              <h5 class="title mb-3">
+                System Automation
+              </h5>
+              <p class="mb-5">
+                Bespoke Websites and Web Apps that are tailored to your individual needs. We listen to you and apply experience and expertise to creating a masterpiece you will be proud to call your own.
+              </p>
+              <div>
+                <a href="" class="cta-btn ">
+                  Bring Idea To Life
+                </a>
+              </div>
+            </div>
+
+          </div>
+
+          <div class="col-md-6 col-lg-4 mb-4 d-flex align-items-center justify-content-center">
+            <div class="d-flex justify-content-center mt-2">
+              <a href="" class="pg-link">
+                See Full Products Catalog
+              </a>
+            </div>
+          </div>
+          
+        </div>
+        
+      </div>
+     
+  </section>
+
+  <!-- Carousel -->
+
+  <section class="py-5">
+      <div class=" bg-light ">
+        <div class="heading d-flex align-items-center mb-5 ">
+            <div class="line"></div>
+            <h3 >We Are Working With</h3>
+        </div>
+        <div class="pb-3">
+          <div class="py-4 row slider">
+            <div class="client-holder col text-center">
+              <img src="assets/images/go-shop.png" alt="GoShop" class="img-fluid mx-auto">
+            </div>
+            <div class="client-holder col text-center">
+              <img src="assets/images/event-venues.png" alt="EventVenues" class="img-fluid mx-auto">
+            </div>
+            <div class="client-holder col text-center">
+              <img src="assets/images/CENTER-LOGO.png" alt="CenterPrices" class="img-fluid mx-auto">
+            </div>
+            <div class="client-holder col text-center">
+              <img src="assets/images/goExpress.png" alt="GoExpress" class="img-fluid mx-auto">
+            </div>
+            <div class="client-holder col text-center">
+              <img src="assets/images/gosourcelogo-new.png" alt="GoSource" class="img-fluid mx-auto">
+            </div>
+            <div class="client-holder col text-center">
+              <img src="assets/images/graphe-academy.png" alt="GrapheAcademy" class="img-fluid mx-auto">
+            </div>
+            <div class="client-holder col text-center">
+              <img src="assets/images/payFast.png" alt="PayFast" class="img-fluid mx-auto">
+            </div>
+          </div>
+          <div class="slider-holder  mb-4">
+  
+            <div class="py-4 row btm-slider ">
+              <div class="client-holder col text-center">
+                <img src="assets/images/go-shop.png" alt="GoShop" class="img-fluid mx-auto">
+              </div>
+              <div class="client-holder col text-center">
+                <img src="assets/images/event-venues.png" alt="EventVenues" class="img-fluid mx-auto">
+              </div>
+              <div class="client-holder col text-center">
+                <img src="assets/images/CENTER-LOGO.png" alt="CenterPrices" class="img-fluid mx-auto">
+              </div>
+              <div class="client-holder col text-center">
+                <img src="assets/images/goExpress.png" alt="GoExpress" class="img-fluid mx-auto">
+              </div>
+              <div class="client-holder col text-center">
+                <img src="assets/images/gosourcelogo-new.png" alt="GoSource" class="img-fluid mx-auto">
+              </div>
+              <div class="client-holder col text-center">
+                <img src="assets/images/graphe-academy.png" alt="GrapheAcademy" class="img-fluid mx-auto">
+              </div>
+              <div class="client-holder col text-center">
+                <img src="assets/images/payFast.png" alt="PayFast" class="img-fluid mx-auto">
+              </div>
+            </div>
+          </div>
+        </div>
+
+        
+      </div>
+     
+  </section>
+
+  <!-- Form -->
+  <section class="why-us-area">
+    <div class="container">
+        <div class="row m-0">
+            <div class="col-md-6">
+                <!-- title div -->
+                <div class="heading d-flex align-items-center mb-2 pl-1">
+                  <div class="line"></div>
+                  <h3 > Why Us?</h3>
+                </div>
+                <!--  -->
+                <div class="col-12 px-0 ">
+                    <div class="media">
+                        <img src="assets/icons/delicate_team.png" class="align-self-center mr-3" alt="...">
+                        <div class="media-body pt-3">
+                          <h5 class="">Dedicated Team</h5>
+                          <p> Highly-skilled and technically capable workforce.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0 ">
+                    <div class="media">
+                        <img src="assets/icons/update_technology.png" class="align-self-center mr-3" alt="...">
+                        <div class="media-body pt-4">
+                          <h5 class="">Updated Technology</h5>
+                          <p> Innovation is our watchword, Operate with trending techs.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0  ">
+                    <div class="media">
+                        <img src="assets/icons/delivery_time.png" class="align-self-center mr-3" alt="...">
+                        <div class="media-body pt-3">
+                          <h5 class="">Delivery on Time</h5>
+                          <p> We deliver successful projects for public and private.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 px-0 ">
+                    <div class="media">
+                        <img src="assets/icons/quality_assurance.png" class="align-self-center mr-3" alt="...">
+                        <div class="media-body pt-3">
+                          <h5 class="">Quality Assurace</h5>
+                          <p> We render first class services to our clients.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div id="contact" class="col-md-6">
+                <div class="heading mx-auto mb-2 px-0">
+                  <h3>Get Started</h3>
+                </div>
+
+                <form class="" method="POST" action="contact.php"> 
+                    <div class="form-row">
+                      <div class="form-group col-lg-6">
+                        <input type="text" name="fname" class="form-control bg-light" required placeholder=" Firstname, Surname*">
+                      </div>
+                      <div class="form-group col-lg-6">
+                        <input type="text" name="cname" Class="form-control bg-light" required placeholder="Company Name*">
+                      </div>
+                    </div>
+
+                    <div class="form-row">
+                      <div class="form-group col-lg-6">
+                        <input type="email" name="email" class="form-control bg-light" required placeholder=" Email Address*">
+                      </div>
+                      <div class="form-group col-lg-6">
+                        <input type="tel" name="phone" Class="form-control bg-light" required placeholder=" Phone Number*">
+                      </div>
+                    </div>
+
+                    <div class="form-row">
+                      <div class="form-group col-lg-6">
+                        <select name="category" class="custom-select bg-light h-100" required>
+                          <option selected disabled value=""> --Project Category--*</option>
+                          <option value="automation">System Automation</option>
+                          <option value="machine-learning">Machine Learning</option>
+                          <option value="ai">Artificial Intelligence</option>
+                          <option value="web">Web Development</option>
+                          <option value="app">App Development</option>
+                        </select>
+                      </div>
+                      <div class="form-group col-lg-6">
+                        <select name="budget" class="custom-select bg-light h-100" required>
+                          <option selected disabled value=""> --Project Budget--*</option>
+                          <option value="less_than_100k">less than 	&#8358;100,000</option>
+                          <option value="100k_to_500k">	&#8358;100,000 to 	&#8358;500,000</option>
+                          <option value="500k_to_1m">&#8358;500,000 to &#8358;1million</option>
+                          <option value="1m_to_5m">&#8358;1million to &#8358;5million</option>
+                          <option value="greater_than_5m">more than &#8358;5million</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <textarea name="description" class="form-control bg-light" required placeholder="Project Description* " name="" id="" cols="30" rows="6"></textarea>
+                    <div class="my-4 d-flex justify-content-center"> 
+                      <button type="submit" class="cta-btn ">
+                        Get A Quote
+                      </button>
+                    </div>
+                </form> 
+            </div>
+        </div>
+
+    </div>
+  </section>
+
+   
+  <?php include "include/footer.html"?>
+  <!-- jquery .js -->
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/slick.min.js"></script>
+  <!-- bootstrap .js -->
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
